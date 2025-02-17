99da2ad4cd600ed896bc2b867f0083b42841f7e6
root <root@ubuntu-22.04-template>
2025-02-17 09:05:35 +0000

    udpate

--- database/migrations/2025_02_16_185719_add_unique_constraint_to_apartments_table.php
+++ database/migrations/2025_02_16_185719_add_unique_constraint_to_apartments_table.php
@@ -9,7 +9,7 @@ return new class extends Migration {
     {
         Schema::table('apartments', function (Blueprint $table) {
             $table->unique(
-                ['company_id', 'complex_id', 'block_id', 'apartment_number'],
+                ['company_id', 'complex_id', 'block_id','building_id', 'apartment_number'],
                 'apartments_company_complex_block_apartment_unique'
             );
         });
