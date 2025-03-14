af97ba7c59220da92e289dff7229bbb5213f5572
gismat.mammadov <gmammadov539@gmail.com>
2025-03-14 17:06:44 +0400

    finance

--- database/migrations/2025_03_14_081748_create_interaccountings_table.php
+++ database/migrations/2025_03_14_081748_create_interaccountings_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('accounting_accounts', function (Blueprint $table) {
+        Schema::create('interaccountings', function (Blueprint $table) {
             $table->id();
             $table->string('debet_account')->comment('Debet hesabı');
             $table->string('credit_account')->comment('Kredit hesabı');
