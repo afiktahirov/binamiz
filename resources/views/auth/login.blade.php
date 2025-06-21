2830d0ebe4f553ee1a7011a666a3a7ca34f58b75
Nabiyevv <maqa.nabiev@gmail.com>
2025-06-21 16:27:35 +0400

    Refactor login page to use Azerbaijani language

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,8 +9,8 @@
                     <div class="col-xl-4 col-lg-5 col-md-6 d-flex flex-column mx-auto">
                         <div class="card card-plain mt-8">
                             <div class="card-header pb-0 text-left bg-transparent">
-                                <h3 class="font-weight-bolder text-info text-gradient">Welcome back</h3>
-                                <p class="mb-0">Enter your email and password to sign in</p>
+                                <h3 class="font-weight-bolder text-info text-gradient">Xoş Gəldiniz</h3>
+                                <p class="mb-0">Daxil olmaq üçün fin kodunuzu və şifrənizi daxil edin</p>
                             </div>
                             <div class="card-body">
                                 <form method="POST" action="{{ route('login') }}">
@@ -29,10 +29,10 @@
                                     </div>
 
                                     <div class="mb-3">
-                                        <label class="form-label">Password</label>
+                                        <label class="form-label">Şifrə</label>
                                         <input id="password" type="password" name="password"
                                             class="form-control @error('password') is-invalid @enderror"
-                                            placeholder="Password" required>
+                                            placeholder="Şifrə" required>
                                         @error('password')
                                             <span class="invalid-feedback d-block" role="alert">
                                                 <strong>{{ $message }}</strong>
@@ -43,12 +43,12 @@
                                     <div class="form-check form-switch mb-3">
                                         <input class="form-check-input" type="checkbox" name="remember" id="remember"
                                             {{ old('remember') ? 'checked' : '' }}>
-                                        <label class="form-check-label" for="remember">Remember me</label>
+                                        <label class="form-check-label" for="remember">Yadda saxla</label>
                                     </div>
 
                                     <div class="text-center">
                                         <button type="submit" class="btn bg-gradient-info w-100 mt-4 mb-0">
-                                            {{ __('Login') }}
+                                            {{ __('Daxil ol') }}
                                         </button>
                                     </div>
                                 </form>
@@ -58,19 +58,19 @@
                                 @if (Route::has('password.request'))
                                     <p class="mb-2 text-sm">
                                         <a href="{{ route('password.request') }}" class="text-info text-gradient font-weight-bold">
-                                            Forgot your password?
+                                            Şifrəni unutmusunuz?
                                         </a>
                                     </p>
                                 @endif
-                        
+
                             </div>
                         </div>
                     </div>
 
                     <div class="col-md-6">
                         <div class="oblique position-absolute top-0 h-100 d-md-block d-none me-n8">
-                            <div class="oblique-image bg-cover position-absolute fixed-top ms-auto h-100 z-index-0 ms-n6"
-                                style="background-image:url('../assets/img/curved-images/curved6.jpg')">
+                            <div class="oblique-image position-absolute fixed-top ms-auto h-100 z-index-0 ms-n6"
+                                style="background-image:url('http://bina.mmmc.az/storage/uploads/complex/d3HGKCnjQjw8G7WFblxzhgbUe1bTNYBpSiktRl8x.png'); background-size: contain; background-repeat: no-repeat;">
                             </div>
                         </div>
                     </div>
